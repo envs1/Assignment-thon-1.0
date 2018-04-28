0c4a125a4121ac2e430041092356fb1ca52227b3
Elisabeth Novas <elisabethnvs1@gmail.com>
2018-04-28 15:36:28 +0000

    first commit

---
+++ assignment-thon.js
@@ -0,0 +1,23 @@
+var dealership ={
+        "Sedan":{
+         price:"$60 a day", 
+        available:"not available",
+        total:0,
+        request:"no",
+        
+        },
+         "Suv" : {
+         price:"70 dollars a day",
+         available:"available",
+         total: 2,
+         request:"yes",
+        },
+         "sports":{
+         price:"$50 a day", 
+         available:"available",
+         available:"10",
+         request:"no",
+        }
+    
+}
+console.log(dealership);
