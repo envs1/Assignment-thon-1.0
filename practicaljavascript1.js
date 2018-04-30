cbf078398efb816681e71addf27ba0f1474dc27d
Elisabeth Novas <elisabethnvs1@gmail.com>
2018-04-30 16:24:00 +0000

    lesson 1 by Elisabeth Novas

---
+++ practicaljavascript1.js
@@ -0,0 +1,8 @@
+//Practical JavaScript Lesson 1 -Elisabeth Novas
+var todos = ['item 1', 'item 2','item 3'];// Array
+console.log('My Todos:', todos);
+todos.push('item 4');
+todos[0]='changed!';
+todos.splice(0, 1);
+
+
